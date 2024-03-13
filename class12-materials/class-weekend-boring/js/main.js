e0d48f89636397f9fa57a46bb5e8f111986eb5ac
James Escobar <escocodeseth@gmail.com>
2024-03-13 14:39:42 -0400

    adding class 12 JS

---
+++ class12-materials/class-weekend-boring/js/main.js
@@ -0,0 +1,20 @@
+document.querySelector('#check').addEventListener('click', check)
+
+function check() {
+
+  let day = document.querySelector('#day').value.toLowerCase();
+
+  //Conditionals go here
+
+  if (day === "tuesday" || day === "Thursday"){
+    alert ('class day!')
+  }else if( day === "Saturday" || day === "Sunday"){
+    alert('weekend!');
+  }else {
+    alert ('boorrrrrinnnngggggg!!!!!!!!');
+  }
+
+
+}
+console.log ()
+
